5b2068cb4a9e26a0a6c518c74d1b37e8a85e0e78
Vinayak MP <vinayakmp007@gmail.com>
2017-05-25 14:34:06 +0530

    changed tts class

--- src/tts/Tts.java
+++ src/tts/Tts.java
@@ -16,31 +16,14 @@ public class Tts {
     /**
      * @param args the command line arguments
      */
-    private static final String VOICENAME_kevin = "kevin";
- private String text; // string to speech
- 
- public Tts(String text) {
-  this.text = text;
- }
- 
- public void speak() {
-  Voice voice;
-  VoiceManager voiceManager = VoiceManager.getInstance();
-  voice = voiceManager.getVoice(VOICENAME_kevin);
-   Voice[] voices = voiceManager.getVoices();
-    for (int i = 0; i < voices.length; i++) {
-        System.out.println("    " + voices[i].getName()
-                           + " (" + voices[i].getDomain() + " domain)");
+    public static void main(String args[]) {
+    Engine a=new Engine("Hello/hai this is/was me/him");
+    while(a.hasNext()){
+    
+    System.out.println(a.next());
     }
-  voice.allocate();
-  voice.speak(text);
- }
- 
- public static void main(String[] args) {
-      System.setProperty("mbrola.base","/home/vinayak/Downloads/mbr301h");
-  String text = "FreeTTS was written by the Sun Microsystems Laboratories "
-    + "Speech Team and is based on CMU's Flite engine.";
-  Tts freeTTS = new Tts(text);
-  freeTTS.speak();
- }
+    
+    
+    
+    }
 }
